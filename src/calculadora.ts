567c0758a7131a1cdb119b85082a5ee5052ea60e
SAMUEL FERREIRA TRINDADE COSTA <samuel.ftcosta@senacsp.edu.br>
2025-10-16 17:19:43 -0300

    adição do projetinho de calculadora

---
+++ src/calculadora.ts
@@ -0,0 +1,23 @@
+let numero1: number = 2;
+let numero2: number = 5;
+
+function somar() {
+    return `A soma dos dois numeros é ${numero1 + numero2}`;
+}
+
+function multiplicar() {
+    return numero1 * numero2;
+}
+
+function subtração() {
+    return numero1 - numero2;
+}
+
+function divisão() {
+    return numero1 / numero2;
+}
+
+somar();
+multiplicar();
+subtração();
+divisão();

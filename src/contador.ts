148d37dbc633a67b0d303139b3dcdd63dd7a6e09
user_acronus <user_acronus@senacsp.edu.br>
2025-10-17 13:31:53 -0300

    Finalização do contador e inicio da lista de compras

--- src/contador.ts
+++ src/contador.ts
@@ -1,2 +1,25 @@
 let contador: number = 0;
 
+console.log("Contador inicial: 0")
+
+function Implementar(): void {
+    contador++;
+    console.log(`Contador: ${contador}`);
+}
+
+function Decrementar(): void {
+    contador--;
+    console.log(`Contador: ${contador}`);
+}
+
+function Resetar(): void {
+    contador = 0;
+    console.log(`Contador: ${contador}`);
+}
+
+Implementar(); 
+Implementar(); 
+Implementar(); 
+Implementar(); 
+Decrementar();
+Resetar();
---
+++ src/listaDecompras.ts
@@ -0,0 +1,9 @@
+//  O QUE FAZER:
+// FUNÇÕES:
+// adicionarItem(item: string) - Adiciona um item na lista     
+// listarItens() - Mostra todos os itens numerados
+// limparLista() - Remove todos os itens
+// VOCÊ PRECISA:
+// 1 array para guardar os itens
+// 3 funções
+// for para mostrar itens numerados
